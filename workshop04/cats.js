19227a66001dbb043e7ef3387cb294597ddffc49
Leonard Flachs <leonard.flachs@gmail.com>
2022-05-05 12:06:25 +0200

    ⚡️  Workshop completed

---
+++ workshop04/cats.js
@@ -0,0 +1,24 @@
+const animalsToAdopt = [
+  {
+    name: "Lucky",
+    picture: "https://placekitten.com/201/297",
+  },
+  {
+    name: "Symba",
+    picture: "https://placekitten.com/200/139",
+  },
+  {
+    name: "Léo",
+    picture: "https://placekitten.com/200/90",
+  },
+  {
+    name: "Milo",
+    picture: "https://placekitten.com/200/194",
+  },
+  {
+    name: "Charly",
+    picture: "https://placekitten.com/200/179",
+  },
+];
+
+export default animalsToAdopt;
--- workshop04/index.js
+++ workshop04/index.js
@@ -1,3 +1,8 @@
+// import cat from "287.jpeg";
+
+import animalsToAdopt from "./cats";
+const header = document.querySelector("#main-header");
+
 function createCard(img, title) {
   const cards = document.querySelector("#cards");
 
@@ -12,7 +17,8 @@ function createCard(img, title) {
   const cardImg = document.createElement("div");
   cardImg.classList.add("card-img");
   cardImg.style.backgroundImage = "url(" + img + ")";
-  //   cardImg.style.backgroundImage = `url(${img})`;
+
+  cardImg.style.backgroundImage = `url(${img})`;
   cardHeader.appendChild(cardImg);
 
   const cardBody = document.createElement("div");
@@ -30,5 +36,34 @@ function createCard(img, title) {
   cardBody.appendChild(cardButton);
 }
 
-createCard("https://placekitten.com/408/287", "Cool cat");
-createCard("https://placekitten.com/409/187", "Nice cat");
+function createAnimalsCards(animals) {
+  const cards = document.querySelector("#cards");
+  cards.innerHTML = "";
+  animals.forEach(function (animal) {
+    console.log(animal);
+    createCard(animal.picture, animal.name);
+  });
+}
+
+function render() {
+  createAnimalsCards(animalsToAdopt);
+  const dropDown = document.createElement("select");
+  header.appendChild(dropDown);
+
+  animalsToAdopt.forEach(function (animal) {
+    const option = document.createElement("option");
+    option.value = animal.name;
+    option.innerText = animal.name;
+    dropDown.appendChild(option);
+  });
+
+  dropDown.addEventListener("change", function (event) {
+    console.log(event.target.value);
+    const newAnimalArray = animalsToAdopt.filter(function (animal) {
+      return animal.name == event.target.value;
+    });
+    createAnimalsCards(newAnimalArray);
+  });
+}
+
+render();
